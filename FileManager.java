f749c202b47bd8f1a139e7c9011adc12644ea642
Destin Groves <groves15@purdue.edu>
2022-11-04 17:03:24 -0400

    the file structure is real!!! data folder with buyer and seller

--- FileManager.java
+++ FileManager.java
@@ -1,8 +1,11 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /** Handles various file management methods for the program
  * @author Destin Groves
  */
 public class FileManager {
-
 
     /** Used to find a file directory for a given User. Useful when writing or reading files.
      * @param username The username associated with the user directory you wish to find
@@ -10,9 +13,13 @@ public class FileManager {
      * @exception UserNotFoundException when there is no directory that corresponds with the given username.
      */
     public String getDirectoryFromUsername(String username) throws UserNotFoundException {
-
-        //TODO: implement method
-        return null;
+        if (Files.exists(Paths.get("data/buyers/"+username))) {
+            return "data/buyers/"+username;
+        } else if (Files.exists(Paths.get("data/sellers/"+username))) {
+            return "data/sellers/"+username;
+        } else {
+            throw new UserNotFoundException("The requested User does not exist!");
+        }
     }
 
     /** Used to generate a directory for new Users.
@@ -21,9 +28,15 @@ public class FileManager {
      * @return true if the directory was created. Returns false if the directory exists, or some other error occurs.
      */
     public boolean generateDirectoryFromUsername(String username, boolean isSeller) {
-
-        //TODO: implement method
-        return false;
-
+        try {
+            if (isSeller) {
+                Files.createDirectory(Paths.get("data/sellers/"+username));
+            } else {
+                Files.createDirectory(Paths.get("data/buyers/"+username));
+            }
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
