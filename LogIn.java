4ff8b38610f684ee5fe2ddfcb23c83d445cda158
adenr4615 <adenr4615@gmail.com>
2022-11-13 18:28:39 -0500

    Debugging.

--- LogIn.java
+++ LogIn.java
@@ -378,7 +378,7 @@ public class LogIn {
      * @param user user's username
      * @param scan Scanner to capture input
      */
-    public static void deleteUser(String user, Scanner scan) {
+    public static boolean deleteUser(String user, Scanner scan) {
         System.out.println("Are you sure you want to delete your account? This action cannot be undone. " +
                 "\nEnter 'yes' to confirm or 'no' to abort.");
         String response = "";
@@ -405,8 +405,10 @@ public class LogIn {
             userInfo.delete();
             File userDirectory = new File("users/" + user);
             userDirectory.delete();
+            return (true);
         } else {
             System.out.println("We're glad you decided to stay!");
+            return (false);
         }
     }
 
@@ -832,7 +834,7 @@ public class LogIn {
             //This is here because the program breaks if a user tries to do a deletion after changing their name in the same run
             while (running) {
                 System.out.println("Would you like to enter messaging or make account changes? " +
-                        "\n1. Messaging " +
+                        "\n1. User Interactions " +
                         "\n2. Account changes");
                 int input = -1;
                 boolean inputTaken = false;
@@ -871,9 +873,10 @@ public class LogIn {
                         }
                     }
                     if (input == 2) {
-                        deleteUser(user, scan);
-                        userDeleted = true;
-                        running = false;
+                        userDeleted = deleteUser(user, scan);
+                        if (userDeleted) {
+                            running = false;
+                        }
                     } else if (input == 1) {
                         user = appendUsername(user, scan);
                     } else if (input == 3) {
